65e9fae33e816bee9736dcf0a31055a192dcbbd9
Cristiano Pacheco <chris.spb25@gmail.com>
2023-02-18 11:16:50 -0300

    Added the failed queue grid

---
+++ app/code/RunAsRoot/MessageQueueRetry/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace RunAsRoot\MessageQueueRetry\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    public const ADMIN_RESOURCE = 'RunAsRoot_MessageQueueRetry::failed_queue';
+
+    public function __construct(
+        Context $context,
+        private PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+    }
+
+    public function execute(): Page
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Failed queue messages'));
+        $resultPage->setActiveMenu(self::ADMIN_RESOURCE);
+        return $resultPage;
+    }
+}
