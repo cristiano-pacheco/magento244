b6433b7c71620af3d3ea983fd3aca8c854b16bc6
Cristiano Pacheco <chris.spb25@gmail.com>
2023-02-21 20:47:44 -0300

    adds mass queue action

--- app/code/RunAsRoot/MessageQueueRetry/Controller/Adminhtml/Message/MassDelete.php
+++ app/code/RunAsRoot/MessageQueueRetry/Controller/Adminhtml/Message/MassDelete.php
@@ -6,17 +6,17 @@ use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\Controller\Result\Redirect;
 use Magento\Framework\Controller\Result\RedirectFactory;
-use RunAsRoot\MessageQueueRetry\Service\PublishMessageToQueueService;
-use RunAsRoot\MessageQueueRetry\Model\ResourceModel\FailedQueue\CollectionFactory;
 use Magento\Ui\Component\MassAction\Filter;
+use RunAsRoot\MessageQueueRetry\Model\ResourceModel\FailedQueue\CollectionFactory;
+use RunAsRoot\MessageQueueRetry\Repository\FailedQueueRepository;
 
-class MassRequeue extends Action
+class MassDelete extends Action
 {
     public const ADMIN_RESOURCE = 'RunAsRoot_MessageQueueRetry::failed_queue';
 
     public function __construct(
         Context $context,
-        private PublishMessageToQueueService $publishMessageToQueueService,
+        private FailedQueueRepository $failedQueueRepository,
         private RedirectFactory $redirectFactory,
         private CollectionFactory $collectionFactory,
         private Filter $filter
@@ -33,13 +33,13 @@ class MassRequeue extends Action
             $collection = $this->filter->getCollection($this->collectionFactory->create());
 
             foreach ($collection as $failedQueueItem) {
-                $this->publishMessageToQueueService->executeByFailedQueue($failedQueueItem);
+                $this->failedQueueRepository->delete($failedQueueItem);
             }
 
-            $this->messageManager->addSuccessMessage(__('Messages queued successfully'));
+            $this->messageManager->addSuccessMessage(__('The messages have been successfully deleted'));
         } catch (\Exception $e) {
             $this->messageManager->addErrorMessage(
-                __('An error occurred while trying to requeue the message: %1', $e->getMessage())
+                __('An error occurred while trying to delete the messages: %1', $e->getMessage())
             );
         }
 
